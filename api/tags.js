e7b14af2801181327a5220dff429d1e66812285a
ivannissimrch <ivannissimrch@gmail.com>
2025-05-29 12:15:21 -0400

    test4

--- api/tags.js
+++ api/tags.js
@@ -1,8 +1,12 @@
 import { readFileSync } from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
-  const filePath = path.resolve("./data/tags.json");
+  const filePath = path.join(__dirname, "../data/tags.json");
   const data = readFileSync(filePath, "utf-8");
   res.setHeader("Content-Type", "application/json");
   res.status(200).send(data);
