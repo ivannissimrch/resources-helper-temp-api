5e53ccf0e7be4acb94b226c3705e2a229f85dbed
ivannissimrch <ivannissimrch@gmail.com>
2025-05-29 11:36:09 -0400

    add package json file

---
+++ api/tags.js
@@ -0,0 +1,9 @@
+import { readFileSync } from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const filePath = path.resolve("./data/tags.json");
+  const data = readFileSync(filePath, "utf-8");
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).send(data);
+}
