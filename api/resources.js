96fc2ccb7ed62428b604b30c1c39362797b3739e
ivannissimrch <ivannissimrch@gmail.com>
2025-05-29 12:19:30 -0400

    tat

--- api/resources.js
+++ api/resources.js
@@ -1,12 +1,8 @@
 import { readFileSync } from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
-  const filePath = path.join(__dirname, "../data/resources.json");
+  const filePath = path.join(process.cwd(), "data", "resources.json");
   const data = readFileSync(filePath, "utf-8");
   res.setHeader("Content-Type", "application/json");
   res.status(200).send(data);
